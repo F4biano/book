23193ae9117f39558dbeb59e34dff64113b7539d
F4biano <a.pires.fabiano@gmail.com>
2022-05-08 00:17:33 -0300

    up

--- helpers/api/users-repo.js
+++ helpers/api/users-repo.js
@@ -47,5 +47,5 @@ function _delete(id) {
 // private helper functions
 
 function saveData() {
-    fs.writeFileSync(`${__dirnam}/data/users.json`, JSON.stringify(users, null, 4));
+    fs.writeFileSync(`${__dirname}/data/users.json`, JSON.stringify(users, null, 4));
 }
